588f840b44f9695227769eba27854ff42c2e8223
Juslin Komenan <komenan.komenan@uvci.edu.ci>
2020-09-11 17:46:50 +0000

    Movies app built with laravel 8

---
+++ resources/views/movies/index.blade.php
@@ -0,0 +1,23 @@
+@extends('layouts.main')
+
+@section('content')
+<div class='container mx-auto px-4 pt-16'>
+    <div class="popular-movies">
+        <h2 class="uppercase tricking-wider text-orange-500 text-lg font-semibold">Popular Movies</h2>
+        <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-5 ld:grid-cols-6 gap-8">
+            @foreach ($popularMovies as $movie)
+                <x-movie-card :movie='$movie' :genres='$genres' />
+            @endforeach
+        </div>
+    </div>
+
+    <div class="nowplaying-movies py-24">
+        <h2 class="uppercase tricking-wider text-orange-500 text-lg font-semibold">Playing Movies</h2>
+        <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-5 ld:grid-cols-6 gap-8">
+            @foreach ($nowPlayingMovies as $movie)
+           <x-movie-card :movie='$movie' :genres='$genres' />
+            @endforeach
+        </div>
+    </div>
+</div>
+@endsection
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.main')
+
+@section('content')
+<div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
+    <h1 class="text-blue-600 bg-red-300">Bonjour tailwind</h1>
+    <p>Bonsjou</p>
+</div>
+@endsection
+
+@section('scripts')
+
+@endsection
